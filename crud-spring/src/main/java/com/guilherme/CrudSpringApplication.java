99ed87802aff38a7d0ac535adade8ad30623f079
J34993R <guialves.oliveira1234@gmail.com>
2022-05-19 14:33:55 -0300

    first commit

---
+++ crud-spring/src/main/java/com/guilherme/CrudSpringApplication.java
@@ -0,0 +1,31 @@
+package com.guilherme;
+
+import com.guilherme.model.Course;
+import com.guilherme.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setCategory("Fullstack");
+			c.setName("Angular + Spring");
+
+			courseRepository.save(c);
+		};
+	}
+}
---
+++ crud-spring/src/main/java/com/guilherme/model/Course.java
@@ -0,0 +1,24 @@
+package com.guilherme.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Course {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 20)
+    private String category;
+}
